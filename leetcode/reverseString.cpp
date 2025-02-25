db418936abfbba711ce94b0b8a28accb12055c19
kolekarom <omkolekar72@gmail.com>
2025-02-25 22:20:50 +0530

    8

---
+++ leetcode/reverseString.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& str) { //taking a vector of char 
+        reverse(str.begin(), str.end()); // Reverse the vector in
+    }
+};
+
+int main() {
+    Solution s; // Create an instance of the Solution class
+    
+    string str = "Hello World"; // Input string
+    vector<char> charVec(str.begin(), str.end()); // Convert string to vector<char>
+
+    s.reverseString(charVec); // Call the function
+
+    string reversedStr(charVec.begin(), charVec.end()); // Convert vector<char> back to string
+    cout << reversedStr << endl; // Print reversed string
+
+    return 0;
+}
