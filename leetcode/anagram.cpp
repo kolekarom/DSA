259d0f614a2fec86f2c801ed815f07ba76ab9e3e
kolekarom <omkolekar72@gmail.com>
2025-02-25 22:00:47 +0530

    7

---
+++ leetcode/anagram.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool isAnagram(string s, string t) { 
+    if (s.length() != t.length()) 
+        return false; 
+
+    sort(s.begin(), s.end()); 
+    sort(t.begin(), t.end()); 
+
+    return s == t; // Directly return the comparison result
+}
+
+int main() {
+    string s, t; 
+    cin >> s >> t; 
+    cout << (isAnagram(s, t) ? "Anagram" : "Not anagrams") << endl; 
+    return 0;
+}
---
+++ leetcode/maxSumArray.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n = 5;
+    int arr[5] = {1,2,3,4,5};
+    int maxSum = INT_MIN;
+    for(int st = 0; st < n; st++){
+        int sum = 0;
+        for (int end = st; end < n; end++){
+            sum += arr[end];
+        }
+        maxSum = max(maxSum, sum);
+            
+
+    }
+    cout << maxSum << endl;
+    return 0;
+
+
+}
