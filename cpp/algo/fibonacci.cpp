0ba0b430d717e7aa3a9d273e0178d88a19f339cd
kolekarom <omkolekar72@gmail.com>
2025-08-23 09:29:45 +0530

    updated

--- cpp/algo/fibonacci.cpp
+++ cpp/algo/fibonacci.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
 #include<algorithm>
 using namespace std;
-
-
 int fib(int n ){
-        if (n == 0) return 0;
+        // Base cases
+        if (n == 0) return 0; // Fibonacci of 0 is 0
         if(n ==1) return 1;
         return fib(n-1 ) + fib(n-2);
 }
-
 int main (){
     int n;
     cout <<"enter n : ";
     cin >> n;
 
     cout << "fibonacci : " << fib(n);
-
-
-
     return 0;
 }
