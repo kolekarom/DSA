f476d620ac0b3e9b623d8e5a59628c3e90bde060
kolekarom <omkolekar72@gmail.com>
2025-08-28 22:58:31 +0530

    binary search

---
+++ cpp/BinaryTrees/code.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+int main(){
+    // vector<int > preorder= {8,5,1,7,10,12};
+    
+
+    return 0;
+}
---
+++ cpp/BinaryTrees/operation.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+int main(){
+    // vector<int > preorder= {8,5,1,7,10,12};
+    Node* root = new Node(8);
+    root->left = new Node(5);
+    root->right = new Node(10);
+    root->left->left = new Node(1);
+    root->left->right = new Node(7);
+
+    cout <<root->data << endl;
+    cout <<root->left->data << endl;
+    cout <<root->right->data << endl;
+    
+
+    return 0;
+}
